4260590f7760d47ec8b0e55d21907eddeb98f7b0
Daniel <daniel.arnstrom@liu.se>
2022-04-16 15:59:38 +0200

    Remove Julia interface (provided in a separate repo).

--- interfaces/DAQP.jl/src/constants.jl
+++
@@ -1,6 +0,0 @@
-const flag2status= Dict{Int,Symbol}(1 => :Optimal,
-								   -1 => :Primal_Infeasible,
-								   -2 => :Cycling,
-								   -3 => :Unbounded,
-								   -4 => :Iteration_Limit
-								   )
