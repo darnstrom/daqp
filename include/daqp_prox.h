87266308d0374cb7d2062bdb9f5ea20fca9c44a9
Daniel Arnström <daniel.arnstrom@liu.se>
2022-07-05 07:54:37 +0200

    Correct typo in daqp_prox.h

--- include/daqp_prox.h
+++ include/daqp_prox.h
@@ -1,32 +1,10 @@
-#ifndef DAQP_BNB_H 
-# define DAQP_BNB_H 
+#ifndef DAQP_PROX_H 
+# define DAQP_PROX_H 
 
 #include "types.h"
 #include "constants.h"
 #include "daqp.h"
 
-int daqp_bnb(DAQPWorkspace *work);
+int daqp_prox(DAQPWorkspace *work);
 
-typedef struct{
-  int* bin_states; 
-}DAQPNode;
-
-typedef struct{
-  int* bin_ids;
-  int nb;
-
-  DAQPNode* tree;
-  int n_tree;
-  int start_id;
-
-  c_float  Jbar;
-  c_float* ubar;
-
-  DAQPWorkspace* dwork;
-}DAQPBnBWorkspace;
-
-int daqp_bnb(DAQPBnBWorkspace* work);
-int get_branch_id(DAQPNode *node, DAQPBnBWorkspace* work);
-int spawn_children(DAQPNode *node, int branch_id, DAQPBnBWorkspace* work);
-
-#endif //ifndef DAQP_BNB_H 
+#endif //ifndef DAQP_PROX_H 
