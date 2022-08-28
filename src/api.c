a2bb765ad31f73ed34c034d75d7ebf5bc20ffae3
Daniel Arnström <daniel.arnstrom@liu.se>
2022-08-28 12:34:32 +0200

    Ensure scaling is used in hiqp.

--- src/api.c
+++ src/api.c
@@ -187,6 +187,13 @@ void setup_daqp_hiqp(DAQPWorkspace* work, int* break_points, int nh){
 
         work->hier->nh = nh;
         work->hier->break_points= break_points;
+
+        // Normalize M
+        // TODO: make scaling handled better generally.
+        if(work->scaling == NULL){
+            work->scaling= malloc(work->m*sizeof(c_float));
+            normalize_M(work);
+        }
     }
 }
 
