14932d65981631216601a87035f638f30e1c4776
Daniel Arnström <daniel.arnstrom@liu.se>
2023-02-03 11:26:53 +0100

    Improvements to LDL_remove.

--- src/factorization.c
+++ src/factorization.c
@@ -51,11 +51,10 @@ void update_LDL_add(DAQPWorkspace *work, const int add_ind){
             j= start_col;
         }
         // Multiply Mk*Mi (NULL signify unity)
-        if(Mk == NULL){ 
-            if(Mi ==NULL) sum = 0;
-            else sum = Mi[j];
-        }
-        else if(Mi == NULL) sum = Mk[j];
+        if(Mk == NULL)
+            sum = (Mi ==NULL) ? 0 : Mi[j];
+        else if(Mi == NULL)
+            sum = Mk[j];
         else
             for(sum = 0;j<NX;j++)
                 sum+=Mk[j]*Mi[j];
@@ -87,105 +86,44 @@ void update_LDL_add(DAQPWorkspace *work, const int add_ind){
         work->D[work->n_active]=0;
     }
 }
-//void update_LDL_remove(DAQPWorkspace *work, const int rm_ind){
-//    if(work->n_active==rm_ind+1)
-//        return;
-//    int i, j, r, old_disp, new_disp, w_count, n_update=work->n_active-rm_ind-1;
-//    c_float* w = &work->zldl[rm_ind]; // zldl will be obsolete => use to allocations
-//    // Extract parts to keep/update in L & D
-//    new_disp=ARSUM(rm_ind);
-//    old_disp=new_disp+(rm_ind+1);
-//    w_count= 0;
-//    // Remove column rm_ind (and add parts of L in its new place)
-//    // I.e., copy row i into i-1
-//    for(i = rm_ind+1;i<work->n_active;old_disp++,new_disp++,i++) //(disp++ skips blank element)..
-//        for(j=0;j<i;j++){
-//            if(j!=rm_ind)
-//                work->L[new_disp++]=work->L[old_disp++];
-//            else
-//                w[w_count++] = work->L[old_disp++];
-//        }
-//    // Algorithm C1 in Gill 1974 for low-rank update of LDL
-//    // (L2 block...)
-//    // TODO the disp can most likely be done cleaner...
-//    c_float p,beta,d_bar,alpha=work->D[rm_ind];
-//    // i - Element/row to update|j - Column which is looped over|r - Row to loop over
-//    old_disp=ARSUM(rm_ind)+rm_ind;
-//    for(j = 0, i=rm_ind; j<n_update;j++,i++){
-//        p=w[j];
-//        d_bar = work->D[i+1]+alpha*p*p;
-//        beta = p*alpha/d_bar;
-//        alpha =work->D[i+1]*alpha/d_bar;
-//        work->D[i] = d_bar;
-//        // This means that singularity was not detected correctly before (numerical erros)
-//        // TODO Do some kind of "repair" step.
-//        if(d_bar<work->settings->zero_tol){
-//            //work->D[i]=0;
-//            work->sing_ind=i;
-//        }
-//        old_disp+=i+1;
-//        for(r=j+1, new_disp=old_disp+j;r<n_update;r++){
-//            w[r] -= p*work->L[new_disp];//instead, initialize new_disp+j
-//            work->L[new_disp]+= beta*w[r]; //Use sum to block register
-//            new_disp+=rm_ind+r+1; //Update to the id which starts the next row in L
-//        }
-//    }
-//}
-
 void update_LDL_remove(DAQPWorkspace *work, const int rm_ind){
     if(work->n_active==rm_ind+1)
         return;
-    int i, j, r, old_disp, new_disp, z_count , n_update=work->n_active-rm_ind-1;
-    c_float* z = &work->zldl[rm_ind]; // zldl will be obsolete => use to allocations
+    int i, j, r, old_disp, new_disp, w_count, n_update=work->n_active-rm_ind-1;
+    c_float* w = &work->zldl[rm_ind]; // zldl will be obsolete => use to allocations
     // Extract parts to keep/update in L & D
     new_disp=ARSUM(rm_ind);
     old_disp=new_disp+(rm_ind+1);
-    z_count = 0;
+    w_count= 0;
     // Remove column rm_ind (and add parts of L in its new place)
     // I.e., copy row i into i-1
     for(i = rm_ind+1;i<work->n_active;old_disp++,new_disp++,i++) //(disp++ skips blank element)..
-        for(j=0;j<i;j++){ 
+        for(j=0;j<i;j++){
             if(j!=rm_ind)
                 work->L[new_disp++]=work->L[old_disp++];
             else
-                z[z_count++] = work->L[old_disp++];
+                w[w_count++] = work->L[old_disp++];
         }
-    // t-method in Fletcher 1974 for low-rank update of LDL
-    c_float d,v,beta,t,tn,alpha,ztemp;
-    t = 1/work->D[rm_ind];
+    // Algorithm C1 in Gill 1974 for low-rank update of LDL
+    // L2 block
+    c_float p,beta,dbar,alpha=work->D[rm_ind];
     // i - Element/row to update|j - Column which is looped over|r - Row to loop over
     old_disp=ARSUM(rm_ind)+rm_ind;
-    for(j = 0, i=rm_ind; j<n_update;j++,i++){
-        v=z[j];
-        d=work->D[i+1];
-        if(d == 0){
-            work->D[i] = v*v/t;
-            break; // Assume singularity only in last element
-        }
-        else{
-            tn= t+v*v/d;
-            alpha = tn/t;
-            work->D[i] = d*alpha;
-            t = tn;
-            beta = (v/d)/t;
-        }
+    for(j = 0, i=rm_ind+1;;j++,i++){
+        p=w[j];
+        dbar = work->D[i]+alpha*p*p;
+        work->D[i-1] = dbar;
 
-        old_disp+=i+1; 
-        if(alpha<4){
-            for(r=j+1, new_disp=old_disp+j;r<n_update;r++){
-                z[r] -= v*work->L[new_disp];
-                work->L[new_disp]+= beta*z[r];
-                new_disp+=rm_ind+r+1;
-            }
-        }
-        else{
-            alpha = 1/alpha; // called gamma in paper
-            for(r=j+1, new_disp=old_disp+j;r<n_update;r++){
-                ztemp = z[r];
-                z[r] -= v*work->L[new_disp];
-                work->L[new_disp] =alpha*work->L[new_disp]+beta*ztemp;
-                new_disp+=rm_ind+r+1;
-            }
+        if(j == n_update) break;
+
+        beta = p*alpha/dbar;
+        alpha =work->D[i]*alpha/dbar;
+
+        old_disp+=i;
+        for(r=j+1, new_disp=old_disp+j;r<n_update;r++){
+            w[r] -= p*work->L[new_disp];
+            work->L[new_disp]+=beta*w[r];
+            new_disp+=rm_ind+r+1; //Update to the id which starts the next row in L
         }
     }
 }
