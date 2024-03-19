21515c146459a9328961d2118c94e85c362702f3
Daniel Arnström <daniel.arnstrom@liu.se>
2024-03-19 13:40:49 +0100

    Post release of wheels

--- interfaces/daqp-python/setup.py
+++ interfaces/daqp-python/setup.py
@@ -39,7 +39,7 @@ cython_ext = Extension('daqp',
         include_dirs=['csrc/include'])
 
 setup(name='daqp',
-        version='0.5.1',
+        version='0.5.1.post1',
         description='DAQP: A dual active-set QP solver',
         url='http://github.com/darnstrom/daqp',
         author='Daniel Arnström',
