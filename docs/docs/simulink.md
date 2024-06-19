b5445c5d9f414b5ddb02d62a3c9c1e4036f3e235
Christopher Schulte <Christopher.Schulte@rwth-aachen.de>
2024-06-19 18:03:58 +0200

    bug fixed, docs updated

--- docs/docs/simulink.md
+++ docs/docs/simulink.md
@@ -10,6 +10,7 @@ math: mathjax3
 
 
 ## Setting up the problem
+
 In Simulink, an s-function block can be used to solve a quadratic program. The s-function needs to link to the compiled c-code called "DAQP_sfunc". To compile the c-code, the following matlab-script can be used in the terminal:
 
 ```matlab
@@ -18,28 +19,24 @@ make_sfunc()
 
 _Note_: Please make sure to have a C-compiler installed on your system and linked in matlab. This can be checked by running ``mex -setup`` in the terminal:
 
-After the c-code has been compiled, a new file called ``DAQP_sfunc.mexw64`` (windows) or ``DAQP_sfunc.mexa64`` (linux,mac) should be found.
+After the c-code has been compiled, a new file called ``DAQP_sfunc.mexw64`` (windows) or ``DAQP_sfunc.mexa64`` (linux, mac) should be found.
 
 The s-function block has the following inputs:
-| Inputs | Description | Original Size | Vectorized Size |
-| --- | --- | --- | --- |
-| $H$ | The Hessian matrix of the quadratic objective function | [$n$ x $n$] | [$n^2$ x 1] |
-| $f$ | The linear part of the objective function | [$n$ x 1] | - |
-| $A$ | The matrix of linear constraints | [$m_g$ x $n$] | [$m_g\cdot n$ x 1] |
-| $b_l$ | The lower bound of the linear constraints | [$m$ x 1] | - |
-| $b_u$ | The upper bound of the linear constraints | [$m$ x 1] | - |
-| sense | The type of the constraints | [$m$ x 1] | - |
+| Inputs | Description | Size | 
+| --- | --- | --- |
+| $H$ | The Hessian matrix of the quadratic objective function | [$n$ x $n$] |
+| $f$ | The linear part of the objective function | [$n$ x 1]
+| $A$ | The matrix of linear constraints | [$m_g$ x $n$]
+| $b_l$ | The lower bound of the linear constraints | [$m$ x 1]
+| $b_u$ | The upper bound of the linear constraints | [$m$ x 1]
+| sense | The type of the constraints | [$m$ x 1]
 
 _Note_: When $b_u$ and $b_l$ have more elements than the number of rows in $A$, the first elements in $b_u$ and $b_l$ are interpreted as simple bounds. 
-
-_Note_: The $A$ matrix (and $H$, however $H$ is typically symmetric) is **row-major-order**, meaning that the first $n$ elements are the first row of $A$, the next $n$ elements are the second row of $A$, and so on.
 
 The following parameters are used to set up the problem and need to be set in the mask of the s-function block:
 
 | Parameters | Description | Size |
 | --- | --- | --- |
-| $n$ | Number of decision variables | 1 |
-| $m_g$ | Number of general constraints | 1 |
 | maxIter | Maximum number of iterations | 1 |
 
 The block has the following outputs:
@@ -47,11 +44,10 @@ The block has the following outputs:
 | Outputs | Description | Size |
 | --- | --- | --- |
 | $x$ | The optimal solution | [$n$ x 1] |
-| $\lambda$ | The optimal Lagrange multipliers | [$m_g$ x 1] |
+| $\lambda$ | The optimal Lagrange multipliers | [$m$ x 1] |
 | $f_{val}$ | The optimal value of the objective function | 1 |
 | exitflag | The exit flag of the solver | 1 |
 | iter | The number of iterations used by the solver | 1 |
-
 
 ## Example
 
--- interfaces/daqp-simulink/DAQP_sfunc.c
+++ interfaces/daqp-simulink/DAQP_sfunc.c
@@ -22,7 +22,7 @@
  * 
  * OUTPUTS:
  * 1. x_opt: Optimal solution of the decision variables [n x 1]
- * 2. lambda: Lagrange multipliers [mg x 1]
+ * 2. lambda: Lagrange multipliers [m x 1]
  * 3. fval: Value of the objective function at the optimal solution [1 x 1]
  * 4. flag: Exit flag [1 x 1]
  * 5. iteration: Number of iterations [1 x 1]
@@ -31,20 +31,65 @@
 #define S_FUNCTION_NAME  DAQP_sfunc
 #define S_FUNCTION_LEVEL 2
 
+// #define __DEBUG__ // uncomment for debugging
+
 /*
  * Need to include simstruc.h for the definition of the SimStruct and
  * its associated macro definitions.
  */
 #include "simstruc.h"
+#include "mex.h"
 
-
-
+/*
+ * Need to include the header file for the DAQP API
+*/
 #include "types.h"
 #include "daqp.h"
 #include "api.h"
+
+
 #include <stdlib.h> // For malloc und free
-#include "mex.h"
-#include <math.h>
+#include <math.h> // For isnan, sqrt
+
+
+#define INFINITY 1.0e20
+static void removeNaN(real_T *x, int_T n) {
+    // Replace NaN values with near infinity
+    int_T i;
+    for (i = 0; i < n; i++) {
+        if (isnan(x[i])) {
+            x[i] = INFINITY;
+        }
+    }
+}
+
+
+static void transposeMatrix(real_T *x_in, c_float *x, int_T n, int_T m) {
+    // Convert Matrix column-first-order to row-first-order
+    // n = number of columns
+    // m = number of rows
+
+    // check if x could be successfully allocated
+    if (x == NULL) {
+        return;
+    }
+
+    int_T i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            x[j*n + i] = (c_float) x_in[i*m + j];
+        }
+    }
+}
+
+static void convertFortranToC(InputRealPtrsType x_in, real_T* x, int nV, int nC )
+{
+	int i,j;
+
+	for ( i=0; i<nC; ++i )
+		for ( j=0; j<nV; ++j )
+			x[i*nV + j] = x_in[i][j];
+}
 
 /* Function: mdlInitializeSizes ===============================================
  * Abstract:
@@ -71,13 +116,13 @@ static void mdlInitializeSizes(SimStruct *S)
    
     // ----------------------------- INPUTS -----------------------------
     if (!ssSetNumInputPorts(S, 6)) return;
-    ssSetInputPortWidth(S, 0, DYNAMICALLY_SIZED ); /* H */
+    ssSetInputPortMatrixDimensions(S, 0, DYNAMICALLY_SIZED, DYNAMICALLY_SIZED);
     ssSetInputPortDirectFeedThrough(S, 0, 1);
 
     ssSetInputPortWidth(S, 1, DYNAMICALLY_SIZED ); /* g */
     ssSetInputPortDirectFeedThrough(S, 1, 1);
     
-    ssSetInputPortWidth(S, 2, DYNAMICALLY_SIZED ); /* A */
+    ssSetInputPortMatrixDimensions(S, 2, DYNAMICALLY_SIZED, DYNAMICALLY_SIZED);
     ssSetInputPortDirectFeedThrough(S, 2, 1);
     
     ssSetInputPortWidth(S, 3, DYNAMICALLY_SIZED ); /* lb */
@@ -113,31 +158,52 @@ static void mdlInitializeSizes(SimStruct *S)
 static void mdlStart(SimStruct *S)
 {
     const mxArray* in_maxIter = ssGetSFcnParam(S, 0);
-    int maxIter = (int)((mxGetPr(in_maxIter))[0]);    // Maximum number of iterations
+    int_T maxIter = (int_T)((mxGetPr(in_maxIter))[0]);    // Maximum number of iterations
 
     if (maxIter <= 0) {
         mexErrMsgTxt( "ERROR (DAQP): Maximum number of iterations must be greater than zero." );
     }
     
-    int_T size_H = ssGetInputPortWidth(S, 0);
+    int_T dim_H = ssGetInputPortNumDimensions(S,0);  
+    int_T* size_H = ssGetInputPortDimensions(S, 0);
     int_T size_g = ssGetInputPortWidth(S, 1);
-    int_T size_A = ssGetInputPortWidth(S, 2);
+    int_T dim_A = ssGetInputPortNumDimensions(S,2);
+    int_T* size_A = ssGetInputPortDimensions(S, 2);
     int_T size_lb = ssGetInputPortWidth(S, 3);
     int_T size_ub = ssGetInputPortWidth(S, 4);
     int_T size_sense = ssGetInputPortWidth(S, 5);
 
 
     // ------------------------- Check for sizes -------------------------
-    if (size_H == 0) mexErrMsgTxt( "ERROR (DAQP): Quadratic term of the objective function H is empty." );
+    int_T n = size_H[0]; // Number of decision variables
+    int_T mg = size_A[0] ; // Number of matrix constraints (lb < Ax < ub)
+    int_T m = size_sense; // Number of constraints
+    int_T ms = m - mg; // Number of simple constraints
+
+    // Check for empty matrices
+    if (dim_H != 2) mexErrMsgTxt( "ERROR (DAQP): Quadratic term of the objective function H must be a matrix." );
+    if (dim_A != 2) mexErrMsgTxt( "ERROR (DAQP): Matrix of the linear constraints A must be a 2D matrix." );
+    if (size_H[0] == 0 || size_H[1] == 0) mexErrMsgTxt( "ERROR (DAQP): Quadratic term of the objective function H is empty." );
+    if (size_A[0] == 0 || size_A[1] == 0) mexErrMsgTxt( "ERROR (DAQP): Matrix of the linear constraints A is empty." );
     if (size_g == 0) mexErrMsgTxt( "ERROR (DAQP): Linear term of the objective function g is empty." );
-    if (size_A == 0) mexErrMsgTxt( "ERROR (DAQP): Matrix of the linear constraints A is empty." );
-    if (size_lb == 0) mexErrMsgTxt( "ERROR (DAQP): Lower bounds b_lower are empty." );
-    if (size_ub == 0) mexErrMsgTxt( "ERROR (DAQP): Upper bounds b_upper are empty." );
+    if (size_lb == 0) mexErrMsgTxt( "ERROR (DAQP): Lower bounds b_lower is empty." );
+    if (size_ub == 0) mexErrMsgTxt( "ERROR (DAQP): Upper bounds b_upper is empty." );
     if (size_sense == 0) mexErrMsgTxt( "ERROR (DAQP): Sense of the constraints is empty." );
-    if (sqrt((real_T)size_H) != (real_T) size_g) mexErrMsgTxt( "ERROR (DAQP): Dimension mismatch between the quadratic term H and the linear term g of the objective function." );
-    if (size_A % size_g != 0) mexErrMsgTxt( "ERROR (DAQP): Dimension mismatch in the matrix of the linear constraints A." );
-    if (size_lb != size_sense) mexErrMsgTxt( "ERROR (DAQP): Dimension mismatch in the lower bounds b_lower or sense." );
-    if (size_ub != size_sense) mexErrMsgTxt( "ERROR (DAQP): Dimension mismatch in the upper bounds b_upper or sense." );
+
+    // Check for dimension mismatch
+    if (size_H[0] != size_H[1]) mexErrMsgTxt( "ERROR (DAQP): Quadratic term H must be a square matrix." );
+    if (size_g != n) mexErrMsgTxt( "ERROR (DAQP): Dimension mismatch between the linear term g of the objective function and the number of decision variables." );
+    if (size_lb != m) mexErrMsgTxt( "ERROR (DAQP): Dimension mismatch in the lower bounds b_lower or sense." );
+    if (size_ub != m) mexErrMsgTxt( "ERROR (DAQP): Dimension mismatch in the upper bounds b_upper or sense." );
+    if (size_A[1] != n) mexErrMsgTxt( "ERROR (DAQP): Dimension 2 of the matrix of the linear constraints A must be equal to the number of decision variables." );
+
+    // Check for invalid sizes
+    if (mg < 1) mexErrMsgTxt( "ERROR (DAQP): Number of general constraints must be greater than zero." );
+    if (ms > n) mexErrMsgTxt( "ERROR (DAQP): Number of simple constraints must be less than or equal to the number of decision variables." );
+
+    #if defined(__DEBUG__)
+    mexPrintf("mdlStart finished.\n");
+    #endif
 }
 
 /* Function: mdlInitializeSampleTimes =========================================
@@ -162,7 +228,7 @@ static void mdlOutputs(SimStruct *S, int_T tid) {
     // ----------------------------- PARAMS -----------------------------
     // Get number of maximum working set iterations
 	const mxArray* in_maxIter = ssGetSFcnParam(S, 0);
-	int maxIter = (int)((mxGetPr(in_maxIter))[0]);	// Maximum number of iterations
+	int_T maxIter = (int_T)((mxGetPr(in_maxIter))[0]);	// Maximum number of iterations
     
 
     // ----------------------------- INPUTS -----------------------------
@@ -174,30 +240,30 @@ static void mdlOutputs(SimStruct *S, int_T tid) {
 	in_ub    = ssGetInputPortRealSignalPtrs(S, 4); // Upper bounds
 	in_sense = ssGetInputPortRealSignalPtrs(S, 5); // Sense of the constraints
     
-    int_T size_H = ssGetInputPortWidth(S, 0); // Size of the quadratic term of the objective function
+    int_T* size_H = ssGetInputPortDimensions(S, 0); // Size of the quadratic term of the objective function
     int_T size_g = ssGetInputPortWidth(S, 1); // Size of the linear term of the objective function
-    int_T size_A = ssGetInputPortWidth(S, 2); // Size of the matrix of the linear constraints
+    int_T* size_A = ssGetInputPortDimensions(S, 2); // Size of the matrix of the linear constraints
     int_T size_lb = ssGetInputPortWidth(S, 3); // Size of the lower bounds
     int_T size_ub = ssGetInputPortWidth(S, 4); // Size of the upper bounds
     int_T size_sense = ssGetInputPortWidth(S, 5); // Size of the sense of the constraints
     
 
     // ------------------------- sizes ------------------------------
-    int n = (int) size_g; // Number of decision variables
-    int mg = (int)((real_T) size_A / (real_T) size_g); // Number of matrix constraints (lb < Ax < ub)
-    int m = (int) size_lb; // Number of constraints
-    int ms = m - mg; // Number of simple constraints
+    int_T n = size_g; // Number of decision variables
+    int_T mg = size_A[0]; // Number of matrix constraints (lb < Ax < ub)
+    int_T m = size_sense; // Number of constraints
+    int_T ms = m - mg; // Number of simple constraints
     
 
     // ----------------------- Create  Work -------------------------
-    c_float *x_opt  = (c_float *) calloc( n, sizeof(c_float) );
-    c_float *lambda = (c_float *) calloc( mg, sizeof(c_float) );
-    c_float *H      = (c_float *) calloc( n*n, sizeof(c_float) );
-	c_float *g      = (c_float *) calloc( n, sizeof(c_float) );
-	c_float *A      = (c_float *) calloc( mg*n, sizeof(c_float) );
-	c_float *lb     = (c_float *) calloc( size_sense, sizeof(c_float) );
-	c_float *ub     = (c_float *) calloc( size_sense, sizeof(c_float) );
-	int *sense      = (int *) calloc( size_sense, sizeof(int) );
+    c_float *x_opt  = (c_float *) calloc( n,          sizeof(c_float) );
+    c_float *lambda = (c_float *) calloc( m,         sizeof(c_float) );
+    c_float *H      = (c_float *) calloc( n*n,        sizeof(c_float) );
+	c_float *g      = (c_float *) calloc( n,          sizeof(c_float) );
+	c_float *A      = (c_float *) calloc( mg*n,       sizeof(c_float) );
+	c_float *lb     = (c_float *) calloc( m,          sizeof(c_float) );
+	c_float *ub     = (c_float *) calloc( m,          sizeof(c_float) );
+	int_T *sense    = (int_T *)   calloc( m,          sizeof(int_T) );
 
 
     // --------------------- CHECK FOR NULL ------------------------
@@ -208,31 +274,22 @@ static void mdlOutputs(SimStruct *S, int_T tid) {
 
     // --------------------- Write to Work ----------------------
     int_T i;
-    if ( H != 0 && size_H == n*n) {
-		for ( i=0; i<size_H; ++i )
-			H[i] = (c_float)(*in_H)[i];
-	}
-    if ( g != 0 && size_g == n) {
-		for ( i=0; i<size_g; ++i )
-			g[i] = (c_float)(*in_g)[i];
-	}
-    if ( A != 0 && size_A == mg*n) {
-		for ( i=0; i<size_A; ++i )
-			A[i] = (c_float)(*in_A)[i];
-	}
-    if ( lb != 0 && size_lb == size_sense) {
-		for ( i=0; i<size_lb; ++i )
-			lb[i] = (c_float)(*in_lb)[i];
-	}
-    if ( ub != 0  && size_ub == size_sense) {
-		for ( i=0; i<size_ub; ++i )
-			ub[i] = (c_float)(*in_ub)[i];
-	}
-    if ( sense != 0 ) {
-		for ( i=0; i<size_sense; ++i )
-			sense[i] = (int)(*in_sense)[i];
-	}	
+    transposeMatrix((real_T *) *in_H, H, n, n); // Convert Matrix column-first-order to row-first-order
+
+    for ( i=0; i<size_g; ++i )
+        g[i] = (c_float)(*in_g)[i];
+
+    transposeMatrix((real_T *) *in_A, A, mg, n); // Convert Matrix column-first-order to row-first-order
+
+	for ( i=0; i<size_lb; ++i )
+		lb[i] = (c_float)(*in_lb)[i];
     
+	for ( i=0; i<size_ub; ++i )
+		ub[i] = (c_float)(*in_ub)[i];
+		
+    for ( i=0; i<size_sense; ++i )
+		sense[i] = (int_T)(*in_sense)[i];
+
 
     // ----------------------------- OUTPUTS -----------------------------
     real_T *out_x_opt   = ssGetOutputPortRealSignal(S, 0);
@@ -249,25 +306,32 @@ static void mdlOutputs(SimStruct *S, int_T tid) {
     
     DAQPSettings settings;
     daqp_default_settings(&settings);
-    settings.iter_limit = (int) maxIter;
+    settings.iter_limit = (int_T) maxIter;
 
 
     // ----------------------------- DAQP -----------------------------
-    DAQPProblem qp = {n,m,ms,H,g,A,ub,lb,sense};
-    
-    daqp_quadprog(&result,&qp,&settings);
+     DAQPProblem qp = {n,m,ms,H,g,A,ub,lb,sense}; 
+     daqp_quadprog(&result,&qp,&settings);
 
 
     // ------------------------- WRITE OUTPUTS -------------------------
-    for ( i=0; i<size_g; ++i )
+    for ( i=0; i<n; ++i )
 		out_x_opt[i] = (real_T)(x_opt[i]);
     
-    for ( i=0; i<mg; ++i )
+    for ( i=0; i<m; ++i )
 		out_lambda[i] = (real_T)(lambda[i]);
     
     out_fval[0] = (real_T)(result.fval);
     out_flag[0] = (real_T)(result.exitflag);
     out_iteration[0] = (real_T)(result.iter);
+    
+
+    // -------------------------- REMOVE NaN -------------------------
+    removeNaN(out_x_opt, (int_T) size_g);
+    removeNaN(out_lambda, mg);
+    removeNaN(out_fval, 1);
+    removeNaN(out_flag, 1);
+    removeNaN(out_iteration, 1);
 
 
     // -------------------------- FREE MEMORY --------------------------
@@ -290,13 +354,12 @@ static void mdlOutputs(SimStruct *S, int_T tid) {
 static void mdlTerminate(SimStruct *S)
 {
     // Free memory
-    // int i;
+    // int_T i;
 	// for ( i=0; i<8; ++i ) {
 	// 	if ( ssGetPWork(S)[i] != 0 )
 	// 		free( ssGetPWork(S)[i] );
 	// }
 }
-
 
 #if defined(MATLAB_MEX_FILE)
 
@@ -327,9 +390,7 @@ static void mdlSetDefaultPortDimensionInfo(SimStruct *S) {
     int_T size_lb = ssGetInputPortWidth(S, 3);
     int_T size_ub = ssGetInputPortWidth(S, 4);
     int_T size_sense = ssGetInputPortWidth(S, 5);
-    
-    int_T mg = (int_T)((real_T) size_A / (real_T) size_g);
-    
+        
     // if (ssGetOutputPortWidth(S, 0) == DYNAMICALLY_SIZED) {
     //     ssSetOutputPortMatrixDimensions(S, 0, size_g, 1 );
     // }
@@ -337,11 +398,22 @@ static void mdlSetDefaultPortDimensionInfo(SimStruct *S) {
     //     ssSetOutputPortMatrixDimensions(S, 1, mg, 1 );
     // }
     if (ssGetOutputPortWidth(S, 0) == DYNAMICALLY_SIZED) {
-        ssSetOutputPortMatrixDimensions(S, 0, 2, 1 );
+        ssSetOutputPortMatrixDimensions(S, 0, size_g, 1 );
     }
     if (ssGetOutputPortWidth(S, 1) == DYNAMICALLY_SIZED) {
-        ssSetOutputPortMatrixDimensions(S, 1, 1, 1 );
+        ssSetOutputPortMatrixDimensions(S, 1, size_sense, 1 );
     }
+
+    #if defined(__DEBUG__)
+    mexPrintf("size_H: %d\n", size_H);
+    mexPrintf("size_g: %d\n", size_g);
+    mexPrintf("size_A: %d\n", size_A);
+    mexPrintf("size_lb: %d\n", size_lb);
+    mexPrintf("size_ub: %d\n", size_ub);
+    mexPrintf("size_sense: %d\n", size_sense);
+    mexPrintf("mg: %d\n", mg);
+    #endif
+
 }
 
 #endif
