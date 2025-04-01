9a593ca6dce000878b780d105f35261a5a308de6
Gianluca Garofalo <gianluca.garofalo@outlook.com>
2025-04-01 21:31:31 +0200

    Extended result structure for Eigen; Updated CMakeLists for multiple tests.

--- interfaces/daqp-eigen/daqp.hpp
+++ interfaces/daqp-eigen/daqp.hpp
@@ -1,17 +1,40 @@
 #include <Eigen/Dense>
 #include "api.h"
 
-DAQPResult daqp_solve(Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> A, Eigen::VectorXd& bu, Eigen::VectorXd& bl, Eigen::VectorXi& break_points) {
-    int cols = A.cols();
-    int rows = A.rows();
+class EigenDAQPResult : public DAQPResult {
+  private:
+    int m_;
+    int n_;
+
+  public:
+    EigenDAQPResult(int m, int n)
+      : m_(m)
+      , n_(n) {
+    }
+
+    Eigen::Map<Eigen::VectorXd> get_x() {
+        return Eigen::Map<Eigen::VectorXd>(x, n_);
+    }
+
+    Eigen::Map<Eigen::VectorXd> get_lam() {
+        return Eigen::Map<Eigen::VectorXd>(lam, m_);
+    }
+};
+
+EigenDAQPResult daqp_solve(Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> A,
+                           Eigen::VectorXd &bu,
+                           Eigen::VectorXd &bl,
+                           Eigen::VectorXi &break_points) {
+    int cols    = A.cols();
+    int rows    = A.rows();
     int n_tasks = break_points.size();
 
     assert(bu.size() == rows);
     assert(bl.size() == rows);
     assert(break_points(Eigen::last) == rows);
 
-    DAQPResult result;
-    result.x = new double[cols];
+    EigenDAQPResult result(rows, cols);
+    result.x   = new double[cols];
     result.lam = NULL;
 
     DAQPProblem qp = {cols, rows, 0, NULL, NULL, A.data(), bu.data(), bl.data(), NULL, break_points.data(), n_tasks};
--- interfaces/daqp-eigen/tests/01_static_func.cpp
+++ interfaces/daqp-eigen/tests/01_static_func.cpp
@@ -2,14 +2,15 @@
 #include <Eigen/Dense>
 #include "daqp.hpp"
 
-int main() {
-    // Task 1: -1 <= x <= 1 
+int main()
+{
+    // Task 1: -1 <= x <= 1
     Eigen::MatrixXd A0 = Eigen::MatrixXd::Identity(3, 3);
     Eigen::VectorXd bu0 = Eigen::VectorXd::Ones(3);
-    Eigen::VectorXd bl0 = - Eigen::VectorXd::Ones(3);
+    Eigen::VectorXd bl0 = -Eigen::VectorXd::Ones(3);
     auto m0 = bu0.size();
 
-    // Task 2: x1+x2+x3 <= 1 
+    // Task 2: x1+x2+x3 <= 1
     Eigen::MatrixXd A1 = (Eigen::MatrixXd(1, 3) << 1, 1, 1).finished();
     Eigen::VectorXd bu1 = Eigen::VectorXd::Ones(1);
     Eigen::VectorXd bl1 = Eigen::VectorXd::Constant(1, -DAQP_INF);
@@ -21,7 +22,7 @@ int main() {
     Eigen::VectorXd bl2 = 0.5 * Eigen::VectorXd::Ones(1);
     auto m2 = bu2.size();
 
-    // Task 4: 10 <= 3*x1+x2-x3 <= 20 
+    // Task 4: 10 <= 3*x1+x2-x3 <= 20
     Eigen::MatrixXd A3 = (Eigen::MatrixXd(1, 3) << 3, 1, -1).finished();
     Eigen::VectorXd bu3 = 20 * Eigen::VectorXd::Ones(1);
     Eigen::VectorXd bl3 = 10 * Eigen::VectorXd::Ones(1);
@@ -33,11 +34,9 @@ int main() {
     Eigen::VectorXi break_points = (Eigen::VectorXi(4) << m0, m0 + m1, m0 + m1 + m2, m0 + m1 + m2 + m3).finished();
 
     // Solve
-    DAQPResult result = daqp_solve(A, bu, bl, break_points);
+    EigenDAQPResult result = daqp_solve(A, bu, bl, break_points);
     std::cout << "Solution: \n";
-    for(uint i = 0; i < A.cols(); ++i)
-        std::cout << result.x[i] << " ";
-    std::cout << std::endl;
+    std::cout << result.get_x().transpose() << std::endl;
     std::cout << "Solve time: " << result.solve_time << " seconds" << std::endl;
 
     return 0;
