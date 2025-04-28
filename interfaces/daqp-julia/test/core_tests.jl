6f18eaf92d6d4deb08f5e4fe9d44eb56b3ccde01
Daniel Arnström <55484604+darnstrom@users.noreply.github.com>
2025-04-29 01:19:27 +0200

    Add tests for Julia (#67)
    * Add test for julia interface
    
    * Start of Julia CI
    
    * Remove name
    
    * Fix indent
    
    * pwd
    
    * rm-pwd
    
    * Run julia
    
    * rm run
    
    * Make libdaqp global
    
    * Directly change DAQP_jll

--- interfaces/daqp-julia/test/core_tests.jl
+++ interfaces/daqp-julia/test/core_tests.jl
@@ -1,8 +1,15 @@
 using LinearAlgebra
 using Random
-using Test
 using DAQPBase
+using DAQP_jll
 include(joinpath(dirname(@__FILE__), "utils.jl"))
+
+# Use local libdaqp if available
+_libdaqp = joinpath(pkgdir(DAQPBase),"libdaqp."*Libc.Libdl.dlext)
+if isfile(_libdaqp)
+    @info "Using local libdaqp"
+    DAQP_jll.libdaqp = _libdaqp
+end
 
 # API Tests
 nQPs,nLPs = 100,100;
@@ -10,30 +17,30 @@ n = 100; m = 500; ms = 50;
 nAct = 80
 kappa = 1e2
 tol = 1e-4
-@testset "Quadprog (C )" begin
-  for nQP in 1:nQPs
-	xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa);
-	x,fval,exitflag,info = quadprog(H,f,A,bupper,blower,sense);
-	@test norm(xref-x) < tol;
-  end
-  # Test quadprog interface by passing settings 
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa);
-  s = settings(DAQPBase.Model(),Dict(:iter_limit=>1))
-  x,fval,exitflag,info = quadprog(H,f,A,bupper,blower,sense;settings=s)
-  @test exitflag == -4
+@testset "Quadprog (C)" begin
+    for nQP in 1:nQPs
+        xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa);
+        x,fval,exitflag,info = quadprog(H,f,A,bupper,blower,sense);
+        @test norm(xref-x) < tol;
+    end
+    # Test quadprog interface by passing settings 
+    xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa);
+    s = settings(DAQPBase.Model(),Dict(:iter_limit=>1))
+    x,fval,exitflag,info = quadprog(H,f,A,bupper,blower,sense;settings=s)
+    @test exitflag == -4
 end
 
 @testset "Quadprog (one-sided)" begin
-  for nQP in 1:10
-	_,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa);
-    blower = fill(-1e30,length(bupper))
-	xref,fval,exitflag,info = quadprog(H,f,A,bupper,blower,sense);
-	x,fval,exitflag,info = quadprog(H,f,A,bupper);
-	@test norm(xref-x) < tol;
-  end
+    for nQP in 1:10
+        _,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa);
+        blower = fill(-1e30,length(bupper))
+        xref,fval,exitflag,info = quadprog(H,f,A,bupper,blower,sense);
+        x,fval,exitflag,info = quadprog(H,f,A,bupper);
+        @test norm(xref-x) < tol;
+    end
 end
 
-@testset "Linprog (C )" begin
+@testset "Linprog (C)" begin
     for nQP in 1:nQPs
         xref,f,A,bupper,blower,sense = generate_test_LP(n,m,ms);
         x,fval,exitflag,info = linprog(f,A,bupper,blower,sense);
@@ -42,36 +49,36 @@ end
 end
 
 @testset "Linprog (one-sided)" begin
-  for nQP in 1:10
-	_,f,A,bupper,blower,sense = generate_test_LP(n,m,ms);
-    blower = fill(-1e30,length(bupper))
-	xref,fval,exitflag,info = linprog(f,A,bupper,blower,sense);
-	x,fval,exitflag,info = linprog(f,A,bupper);
-    @test abs(f'*(xref-x)) < tol;
-  end
+    for nQP in 1:10
+        _,f,A,bupper,blower,sense = generate_test_LP(n,m,ms);
+        blower = fill(-1e30,length(bupper))
+        xref,fval,exitflag,info = linprog(f,A,bupper,blower,sense);
+        x,fval,exitflag,info = linprog(f,A,bupper);
+        @test abs(f'*(xref-x)) < tol;
+    end
 end
 
 @testset "Quadprog (JL)" begin
-  qpj = DAQPBase.QPj()
-  for selection_rule in [DAQPBase.DANTZIG, DAQPBase.BLAND]
-      for nQP in 1:10
-          xref,H,f,A,bupper,blower,sense = generate_test_QP(20,100,0,16,1e2);
-          x,lam,AS,J,iter= DAQPBase.daqp_jl(H,f,[A;-A],[bupper;-blower],[sense;sense],Int64[];selection_rule);
-          @test norm(xref-x) < tol;
-      end
-  end
-  # Test warm start
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(20,100,0,16,1e2);
-  x,lam,AS,J,iter= DAQPBase.daqp_jl(H,f,[A;-A],[bupper;-blower],[sense;sense],Int64[1]);
-  @test norm(xref-x) < tol;
-  # Test infeasible problem
-  x,lam,AS,J,iter= DAQPBase.daqp_jl([1.0 0; 0 1],zeros(2),[1.0 0;-1 0],[1;-2],zeros(Cint,2),Int64[]);
-  @test isinf(J)
-  # Test unconstrained problem
-  x,lam,AS,J,iter= DAQPBase.daqp_jl((H=[1.0 0; 0 1], f=zeros(2),
-								 A=[1.0 0;0 1], b=[1;1],
-								 senses=zeros(Cint,2)),Int64[]);
-  @test isempty(AS)
+    qpj = DAQPBase.QPj()
+    for selection_rule in [DAQPBase.DANTZIG, DAQPBase.BLAND]
+        for nQP in 1:10
+            xref,H,f,A,bupper,blower,sense = generate_test_QP(20,100,0,16,1e2);
+            x,lam,AS,J,iter= DAQPBase.daqp_jl(H,f,[A;-A],[bupper;-blower],[sense;sense],Int64[];selection_rule);
+            @test norm(xref-x) < tol;
+        end
+    end
+    # Test warm start
+    xref,H,f,A,bupper,blower,sense = generate_test_QP(20,100,0,16,1e2);
+    x,lam,AS,J,iter= DAQPBase.daqp_jl(H,f,[A;-A],[bupper;-blower],[sense;sense],Int64[1]);
+    @test norm(xref-x) < tol;
+    # Test infeasible problem
+    x,lam,AS,J,iter= DAQPBase.daqp_jl([1.0 0; 0 1],zeros(2),[1.0 0;-1 0],[1;-2],zeros(Cint,2),Int64[]);
+    @test isinf(J)
+    # Test unconstrained problem
+    x,lam,AS,J,iter= DAQPBase.daqp_jl((H=[1.0 0; 0 1], f=zeros(2),
+                                       A=[1.0 0;0 1], b=[1;1],
+                                       senses=zeros(Cint,2)),Int64[]);
+    @test isempty(AS)
 end
 
 @testset "BnB" begin
@@ -96,50 +103,50 @@ end
 end
 
 @testset "Model interface" begin
-  # Setup model and solve problem
-  d = DAQPBase.Model()
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
-  setup(d,H,f,A,bupper,blower,sense)
-  x,fval,exitflag,info = solve(d)
-  @test norm(xref-x) < tol
-  @test norm(H*x+[I(n)[1:ms,:];A]'*info.λ+f) < tol
+    # Setup model and solve problem
+    d = DAQPBase.Model()
+    xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
+    setup(d,H,f,A,bupper,blower,sense)
+    x,fval,exitflag,info = solve(d)
+    @test norm(xref-x) < tol
+    @test norm(H*x+[I(n)[1:ms,:];A]'*info.λ+f) < tol
 
-  # Test access settings
-  s = settings(d)
-  @test s.primal_tol==1e-6
-  settings(d,Dict(:primal_tol=>1e-5))
-  s = settings(d)
-  @test s.primal_tol==1e-5
-  
-  # Update existing model with new problem
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
-  update(d,H,f,A,bupper,blower,sense)
-  x,fval,exitflag,info = solve(d)
-  @test norm(xref-x) < tol
-  DAQPBase.delete!(d); # Test manually running destructor
+    # Test access settings
+    s = settings(d)
+    @test s.primal_tol==1e-6
+    settings(d,Dict(:primal_tol=>1e-5))
+    s = settings(d)
+    @test s.primal_tol==1e-5
+
+    # Update existing model with new problem
+    xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
+    update(d,H,f,A,bupper,blower,sense)
+    x,fval,exitflag,info = solve(d)
+    @test norm(xref-x) < tol
+    DAQPBase.delete!(d); # Test manually running destructor
 end
 
 @testset "C LDP interface" begin
-  # Setup model and solve problem
-  n = 10; m = 50; ms = 5; nAct =0;
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
-  p=DAQPBase.setup_c_workspace(n)
-  A = A'[:,:] # since row major...
-  DAQPBase.init_c_workspace_ldp(p,A,bupper,blower,sense;max_radius=1e30) 
-  @test isfeasible(p,m,ms)
-  bupper[1] = -1e30 #Make trivially infeasible
-  @test !isfeasible(p,m,ms;validate=true)
-  work = unsafe_load(Ptr{DAQPBase.Workspace}(p));
-  @test work.n == n
-  DAQPBase.free_c_workspace(p)
+    # Setup model and solve problem
+    n = 10; m = 50; ms = 5; nAct =0;
+    xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
+    p=DAQPBase.setup_c_workspace(n)
+    A = A'[:,:] # since row major...
+    DAQPBase.init_c_workspace_ldp(p,A,bupper,blower,sense;max_radius=1e30) 
+    @test isfeasible(p,m,ms)
+    bupper[1] = -1e30 #Make trivially infeasible
+    @test !isfeasible(p,m,ms;validate=true)
+    work = unsafe_load(Ptr{DAQPBase.Workspace}(p));
+    @test work.n == n
+    DAQPBase.free_c_workspace(p)
 end
 
 @testset "Code generation" begin
-  n = 5; m = 5; ms = 5; nAct =2;
-  xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
-  sense[1] = DAQPBase.BINARY
-  d = DAQPBase.Model()
-  DAQPBase.setup(d,H,f,A,bupper,blower,sense)
-  DAQPBase.codegen(d,dir="codegen",src=true)
-  rm("codegen",recursive=true)
+    n = 5; m = 5; ms = 5; nAct =2;
+    xref,H,f,A,bupper,blower,sense = generate_test_QP(n,m,ms,nAct,kappa)
+    sense[1] = DAQPBase.BINARY
+    d = DAQPBase.Model()
+    DAQPBase.setup(d,H,f,A,bupper,blower,sense)
+    DAQPBase.codegen(d,dir="codegen",src=true)
+    rm("codegen",recursive=true)
 end
--- interfaces/daqp-julia/test/runtests.jl
+++ interfaces/daqp-julia/test/runtests.jl
@@ -1,4 +1,5 @@
 using Test
-@testset "Core" begin
+
+@testset verbose = true "DAQP Tests" begin
     include("core_tests.jl")
 end
