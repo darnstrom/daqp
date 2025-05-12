4fcea3f9e778577248a2c69b08fef45897e3fb13
Daniel Arnström <daniel.arnstrom@liu.se>
2025-05-12 17:09:33 +0200

    Add header guard in hierarchical for cpp

--- include/hierarchical.h
+++ include/hierarchical.h
@@ -1,10 +1,18 @@
 #ifndef DAQP_HIERARCHICAL_H 
 # define DAQP_HIERARCHICAL_H
 
+# ifdef __cplusplus
+extern "C" {
+# endif // ifdef __cplusplus
+
 #include "types.h"
 #include "constants.h"
 #include "daqp.h"
 
 int daqp_hiqp(DAQPWorkspace *work);
+
+# ifdef __cplusplus
+}
+# endif // ifdef __cplusplus
 
 #endif //ifndef DAQP_HIERARCHICAL_H
