9c2e40fb577ab6dc20ac3e9b5b6f7aa1734cd8cc
Gianluca Garofalo <gianluca.garofalo@outlook.com>
2025-10-25 11:13:34 +0200

    Get slack variables

--- interfaces/daqp-eigen/daqp.cpp
+++ interfaces/daqp-eigen/daqp.cpp
@@ -8,7 +8,8 @@ EigenDAQPResult::EigenDAQPResult()
 
 EigenDAQPResult::EigenDAQPResult(int n, int m)
   : x_{Eigen::VectorXd(n)}
-  , lam_{Eigen::VectorXd(m)} {
+  , lam_{Eigen::VectorXd(m)}
+  , slack_{Eigen::VectorXd(m)} {
     x   = x_.data();
     lam = lam_.data();
 }
@@ -21,6 +22,7 @@ void EigenDAQPResult::resize_primal(int n) {
 void EigenDAQPResult::resize_dual(int m) {
     lam_.resize(m);
     lam = lam_.data();
+    slack_.resize(m);
 }
 
 Eigen::VectorXd EigenDAQPResult::get_primal() const {
@@ -29,6 +31,10 @@ Eigen::VectorXd EigenDAQPResult::get_primal() const {
 
 Eigen::VectorXd EigenDAQPResult::get_dual() const {
     return lam_;
+}
+
+Eigen::VectorXd EigenDAQPResult::get_slack() const {
+    return slack_;
 }
 
 Eigen::VectorXi EigenDAQPResult::get_active_set() const {
@@ -44,6 +50,32 @@ Eigen::VectorXi EigenDAQPResult::get_active_set() const {
     return active_set;
 }
 
+void EigenDAQPResult::set_slack(Eigen::VectorXd const& slack) {
+    slack_ = slack;
+}
+
+
+Eigen::VectorXd compute_slacks(Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> const& matrix,
+                               Eigen::VectorXd const& upper,
+                               Eigen::VectorXd const& lower,
+                               Eigen::VectorXd const& primal) {
+    // For DAQP first ms entries are simple bounds (primal values), rest are A*primal
+    int n  = primal.size();
+    int m  = upper.size();
+    int ms = m - matrix.rows();
+
+    Eigen::VectorXd values(m);
+    values.head(ms) = primal.head(ms);
+    if (matrix.rows() > 0) {
+        values.tail(matrix.rows()) = matrix * primal;
+    }
+
+    auto slack_lw         = (values - lower).array();
+    auto slack_up         = (values - upper).array();
+    Eigen::VectorXd slack = (slack_lw < 0).select(slack_lw, (slack_up > 0).select(slack_up, 0));
+    return slack;
+}
+
 
 EigenDAQPResult daqp_solve(Eigen::MatrixXd& H,
                            Eigen::VectorXd& f,
@@ -57,11 +89,13 @@ EigenDAQPResult daqp_solve(Eigen::MatrixXd& H,
     int ms      = m - A.rows();
     int n_tasks = break_points.size();
 
-    double *H_ptr  = H.size() == 0 ? nullptr : H.data();
-    double *f_ptr  = f.size() == 0 ? nullptr : f.data();
-    double *A_ptr  = A.size() == 0 ? nullptr : A.data();
-    int *sense_ptr = sense.size() == 0 ? nullptr : sense.data();
-    int *bp_ptr    = break_points.size() == 0 ? nullptr : break_points.data();
+    double* H_ptr  = H.size() == 0 ? nullptr : H.data();
+    double* f_ptr  = f.size() == 0 ? nullptr : f.data();
+    double* A_ptr  = A.size() == 0 ? nullptr : A.data();
+    double* bu_ptr = bu.size() == 0 ? nullptr : bu.data();
+    double* bl_ptr = bl.size() == 0 ? nullptr : bl.data();
+    int* sense_ptr = sense.size() == 0 ? nullptr : sense.data();
+    int* bp_ptr    = break_points.size() == 0 ? nullptr : break_points.data();
 
     assert(bu.size() == bl.size());
     assert(ms <= n);
@@ -69,9 +103,10 @@ EigenDAQPResult daqp_solve(Eigen::MatrixXd& H,
 
     EigenDAQPResult result(n, m);
 
-    DAQPProblem qp = {n, m, ms, H_ptr, f_ptr, A_ptr, bu.data(), bl.data(), sense_ptr, bp_ptr, n_tasks};
+    DAQPProblem qp = {n, m, ms, H_ptr, f_ptr, A_ptr, bu_ptr, bl_ptr, sense_ptr, bp_ptr, n_tasks};
     daqp_quadprog(&result, &qp, nullptr);
 
+    result.set_slack(compute_slacks(A, bu, bl, result.get_primal()));
     return result;
 }
 
@@ -108,24 +143,30 @@ DAQP::DAQP(int max_variables, int max_constraints, int max_constraints_in_level)
     work_.settings = &settings_;
 }
 
-DAQP::~DAQP(){
+DAQP::~DAQP() {
     work_.settings = nullptr;
     free_daqp_workspace(&work_);
     free_daqp_ldp(&work_);
 }
 
-int DAQP::resize_result(const int n, const int m, Eigen::VectorXi& break_points){
+int DAQP::resize_result(int const n, int const m, Eigen::VectorXi const& break_points) {
     // Ensure that the new dimensions is not too large
-    if (n > max_variables_) return 1;
-    if (m > max_constraints_) return 2;
+    if (n > max_variables_) {
+        return 1;
+    }
+    if (m > max_constraints_) {
+        return 2;
+    }
 
     // Check if maximum soft constraints exceeds limit
     int ns = 0;
-    for(int i = 0, prev =0; i < break_points.size(); i++){
-        ns = (ns  > break_points(i)- prev) ? ns : break_points(i)-prev;
+    for (int i = 0, prev = 0; i < break_points.size(); i++) {
+        ns   = (ns > break_points(i) - prev) ? ns : break_points(i) - prev;
         prev = break_points(i);
     }
-    if (ns > max_constraints_in_level_) return 3;
+    if (ns > max_constraints_in_level_) {
+        return 3;
+    }
 
     // Resize primal and dual variables
     if (result_.x == nullptr) {
@@ -142,15 +183,14 @@ int DAQP::resize_result(const int n, const int m, Eigen::VectorXi& break_points)
 }
 
 // Update the workspace with a new problem
-int DAQP::update(Eigen::MatrixXd& H,
-                 Eigen::VectorXd& f,
-                 Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> A,
-                 Eigen::VectorXd& bu,
-                 Eigen::VectorXd& bl,
-                 Eigen::VectorXi& sense,
-                 Eigen::VectorXi& break_points,
+int DAQP::update(Eigen::MatrixXd const& H,
+                 Eigen::VectorXd const& f,
+                 Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> const& A,
+                 Eigen::VectorXd const& bu,
+                 Eigen::VectorXd const& bl,
+                 Eigen::VectorXi const& sense,
+                 Eigen::VectorXi const& break_points,
                  int update_mask) {
-
     int n       = A.cols();
     int m       = bu.size();
     int ms      = m - A.rows();
@@ -160,21 +200,22 @@ int DAQP::update(Eigen::MatrixXd& H,
     assert(ms <= n);
     assert(n_tasks == 0 || break_points(Eigen::last) == m);
 
-    // Get correct pointers
-    double *H_ptr  = H.size() == 0 ? nullptr : H.data();
-    double *f_ptr  = f.size() == 0 ? nullptr : f.data();
-    double *A_ptr  = A.size() == 0 ? nullptr : A.data();
-    int *sense_ptr = sense.size() == 0 ? nullptr : sense.data();
-    int *bp_ptr    = break_points.size() == 0 ? nullptr : break_points.data();
+    // Get correct pointers (using const_cast because DAQP C API expects non-const pointers but doesn't modify the data)
+    double* H_ptr  = H.size() == 0 ? nullptr : const_cast<double*>(H.data());
+    double* f_ptr  = f.size() == 0 ? nullptr : const_cast<double*>(f.data());
+    double* A_ptr  = A.size() == 0 ? nullptr : const_cast<double*>(A.data());
+    double* bu_ptr = bu.size() == 0 ? nullptr : const_cast<double*>(bu.data());
+    double* bl_ptr = bl.size() == 0 ? nullptr : const_cast<double*>(bl.data());
+    int* sense_ptr = sense.size() == 0 ? nullptr : const_cast<int*>(sense.data());
+    int* bp_ptr    = break_points.size() == 0 ? nullptr : const_cast<int*>(break_points.data());
 
     //
-    int resize_status = resize_result(n,m,break_points);
+    int resize_status = resize_result(n, m, break_points);
     assert(resize_status == 0);
 
-    DAQPProblem qp = {n, m, ms, H_ptr, f_ptr, A_ptr, bu.data(), bl.data(), sense_ptr, bp_ptr, n_tasks};
-    qp_ = qp; // To remember the qp
+    qp_ = {n, m, ms, H_ptr, f_ptr, A_ptr, bu_ptr, bl_ptr, sense_ptr, bp_ptr, n_tasks};
 
-    if(update_mask < 0){
+    if (update_mask < 0) {
         // Assume that everythig should be updated
         update_mask = UPDATE_Rinv + UPDATE_M + UPDATE_v + UPDATE_d + UPDATE_sense + UPDATE_hierarchy;
     }
@@ -183,43 +224,95 @@ int DAQP::update(Eigen::MatrixXd& H,
 }
 
 // Solve the LDP that is in the workspace
-const EigenDAQPResult& DAQP::solve(){
-    daqp_solve(&result_,&work_);
+EigenDAQPResult const& DAQP::solve() {
+    daqp_solve(&result_, &work_);
+
+    Eigen::Map<const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> matrix(
+      qp_.A, qp_.m, qp_.n);
+    Eigen::Map<const Eigen::VectorXd> upper(qp_.bupper, qp_.m);
+    Eigen::Map<const Eigen::VectorXd> lower(qp_.blower, qp_.m);
+
+    result_.set_slack(compute_slacks(matrix, upper, lower, result_.get_primal()));
     return result_;
 }
 
 // Solve min_x ||x||
 //        s.t  bl <= A*x <= bu
-const EigenDAQPResult& DAQP::solve(Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> A,
-                                   Eigen::VectorXd& bu,
-                                   Eigen::VectorXd& bl,
-                                   Eigen::VectorXi& break_points) {
+EigenDAQPResult const& DAQP::solve(Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> const& A,
+                                   Eigen::VectorXd const& bu,
+                                   Eigen::VectorXd const& bl,
+                                   Eigen::VectorXi const& break_points) {
     Eigen::MatrixXd H(0, 0);
     Eigen::VectorXd f(0);
     Eigen::VectorXi sense(0);
-    update(H,f,A,bu,bl,sense,break_points,UPDATE_M + UPDATE_d + UPDATE_sense + UPDATE_hierarchy);
+    update(H, f, A, bu, bl, sense, break_points, UPDATE_M + UPDATE_d + UPDATE_sense + UPDATE_hierarchy);
     return solve();
 }
 
-void DAQP::set_primal_tol(double val)  { settings_.primal_tol = val;}
-void DAQP::set_dual_tol(double val)    { settings_.dual_tol = val;}
-void DAQP::set_zero_tol(double val)    { settings_.zero_tol = val;}
-void DAQP::set_pivot_tol(double val)   { settings_.pivot_tol = val;}
-void DAQP::set_progress_tol(double val){ settings_.progress_tol = val;}
-void DAQP::set_cycle_tol(int val)      { settings_.cycle_tol = val;}
-void DAQP::set_iter_limit(int val)     { settings_.iter_limit = val;}
-void DAQP::set_fval_bound(double val)  { settings_.fval_bound = val;}
-void DAQP::set_eps_prox(double val)    { settings_.eps_prox = val;}
-void DAQP::set_eta_prox(double val)    { settings_.eta_prox = val;}
-void DAQP::set_rho_soft(double val)    { settings_.rho_soft = val;}
-void DAQP::set_rel_subopt(double val)  { settings_.rel_subopt = val;}
-void DAQP::set_abs_subopt(double val)  { settings_.abs_subopt = val;}
-void DAQP::set_sing_tol(double val)  { settings_.sing_tol = val;}
-void DAQP::set_refactor_tol(double val)  { settings_.refactor_tol = val;}
+void DAQP::set_primal_tol(double val) {
+    settings_.primal_tol = val;
+}
+void DAQP::set_dual_tol(double val) {
+    settings_.dual_tol = val;
+}
+void DAQP::set_zero_tol(double val) {
+    settings_.zero_tol = val;
+}
+void DAQP::set_pivot_tol(double val) {
+    settings_.pivot_tol = val;
+}
+void DAQP::set_progress_tol(double val) {
+    settings_.progress_tol = val;
+}
+void DAQP::set_cycle_tol(int val) {
+    settings_.cycle_tol = val;
+}
+void DAQP::set_iter_limit(int val) {
+    settings_.iter_limit = val;
+}
+void DAQP::set_fval_bound(double val) {
+    settings_.fval_bound = val;
+}
+void DAQP::set_eps_prox(double val) {
+    settings_.eps_prox = val;
+}
+void DAQP::set_eta_prox(double val) {
+    settings_.eta_prox = val;
+}
+void DAQP::set_rho_soft(double val) {
+    settings_.rho_soft = val;
+}
+void DAQP::set_rel_subopt(double val) {
+    settings_.rel_subopt = val;
+}
+void DAQP::set_abs_subopt(double val) {
+    settings_.abs_subopt = val;
+}
+void DAQP::set_sing_tol(double val) {
+    settings_.sing_tol = val;
+}
+void DAQP::set_refactor_tol(double val) {
+    settings_.refactor_tol = val;
+}
 
-Eigen::VectorXd DAQP::get_primal() {return result_.get_primal();}
-Eigen::VectorXd DAQP::get_dual()   {return result_.get_dual();}
-Eigen::VectorXi DAQP::get_active_set() {return result_.get_active_set();}
-int             DAQP::get_status() {return result_.exitflag;}
-int             DAQP::get_iterations() {return result_.iter;}
-double          DAQP::get_solve_time() {return result_.solve_time;}
+Eigen::VectorXd DAQP::get_primal() {
+    return result_.get_primal();
+}
+Eigen::VectorXd DAQP::get_dual() {
+    return result_.get_dual();
+}
+Eigen::VectorXd DAQP::get_slack() {
+    return result_.get_slack();
+}
+Eigen::VectorXi DAQP::get_active_set() {
+    return result_.get_active_set();
+}
+int DAQP::get_status() {
+    return result_.exitflag;
+}
+int DAQP::get_iterations() {
+    return result_.iter;
+}
+double DAQP::get_solve_time() {
+    return result_.solve_time;
+}
---
+++ interfaces/daqp-eigen/tests/04_slack_sign.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <daqp.hpp>
+
+bool verify_slack(EigenDAQPResult const& result) {
+    Eigen::VectorXd dual  = result.get_dual();
+    Eigen::VectorXd slack = result.get_slack();
+    auto same_sign        = (dual.array() * slack.array()) >= 0;
+    return same_sign.all();
+}
+
+int main() {
+    // Task 1: -1 <= x <= 1
+    Eigen::MatrixXd A0  = Eigen::MatrixXd::Identity(3, 3);
+    Eigen::VectorXd bu0 = Eigen::VectorXd::Ones(3);
+    Eigen::VectorXd bl0 = -Eigen::VectorXd::Ones(3);
+
+    // Task 2: x1+x2+x3 <= 1
+    Eigen::MatrixXd A1  = (Eigen::MatrixXd(1, 3) << 1, 1, 1).finished();
+    Eigen::VectorXd bu1 = Eigen::VectorXd::Ones(1);
+    Eigen::VectorXd bl1 = Eigen::VectorXd::Constant(1, -DAQP_INF);
+
+    // Task 3: x1 - x2 == 0.5
+    Eigen::MatrixXd A2  = (Eigen::MatrixXd(1, 3) << 1, -1, 0).finished();
+    Eigen::VectorXd bu2 = 0.5 * Eigen::VectorXd::Ones(1);
+    Eigen::VectorXd bl2 = 0.5 * Eigen::VectorXd::Ones(1);
+
+    // Task 4: 10 <= 3*x1+x2-x3 <= 20
+    Eigen::MatrixXd A3  = (Eigen::MatrixXd(1, 3) << 3, 1, -1).finished();
+    Eigen::VectorXd bu3 = 20 * Eigen::VectorXd::Ones(1);
+    Eigen::VectorXd bl3 = 10 * Eigen::VectorXd::Ones(1);
+
+    // Stack the tasks
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> A =
+      (Eigen::MatrixXd(6, 3) << A0, A1, A2, A3).finished();
+    Eigen::VectorXd bu = (Eigen::VectorXd(6) << bu0, bu1, bu2, bu3).finished();
+    Eigen::VectorXd bl = (Eigen::VectorXd(6) << bl0, bl1, bl2, bl3).finished();
+
+    Eigen::VectorXi break_points = (Eigen::VectorXi(4) << 3, 4, 5, 6).finished();
+
+    // Test static function
+    auto result = daqp_solve(A, bu, bl, break_points);
+    bool test1  = verify_slack(result);
+
+    // Test DAQP class
+    DAQP solver(3, 50, 5);
+    result     = solver.solve(A, bu, bl, break_points);
+    bool test2 = verify_slack(result);
+
+    std::cout << "Static function: " << (test1 ? "PASS" : "FAIL") << std::endl;
+    std::cout << "Class method: " << (test2 ? "PASS" : "FAIL") << std::endl;
+
+    return (test1 && test2) ? 0 : 1;
+}
