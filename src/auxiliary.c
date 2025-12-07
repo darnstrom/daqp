09549313732a7e6ecedc282cc9cf96e7df7dd69d
Daniel Arnström <55484604+darnstrom@users.noreply.github.com>
2025-12-07 18:22:34 +0100

    Fix indentation

--- src/auxiliary.c
+++ src/auxiliary.c
@@ -365,7 +365,7 @@ void pivot_last(DAQPWorkspace *work){
 int activate_constraints(DAQPWorkspace *work){
     //TODO prioritize inequalities?
     int i;
-    for(i =0;i<N_CONSTR ;i++){
+    for(i =0;i<N_CONSTR;i++){
         if(IS_ACTIVE(i)){
 #ifdef SOFT_WEIGHTS
             if(IS_LOWER(i)){
