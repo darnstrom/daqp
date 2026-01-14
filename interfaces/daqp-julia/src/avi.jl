aa09d8091c1ee710ccdb6f1062aaa3351ece8d17
Daniel Arnström <daniel.arnstrom@gmail.com>
2026-01-14 20:05:45 +0100

    Introduce AVI workspace to allow for hot starts

--- interfaces/daqp-julia/src/avi.jl
+++ interfaces/daqp-julia/src/avi.jl
@@ -1,3 +1,101 @@
+Base.@kwdef mutable struct AVISettings 
+    iter_limit::Int = 1000
+    inner_iter_limit::Int = 10000
+    primal_tol::Float64 = 1e-6
+    dual_tol::Float64 = 1e-12
+    alpha::Float64 = 1.0
+    beta::Float64 = 0.25
+end
+
+struct AVIWorkspace
+    H::Matrix{Float64}
+    f::Vector{Float64}
+    A::Matrix{Float64}
+    bu::Vector{Float64}
+    bl::Vector{Float64}
+
+    H1pI::Matrix{Float64}
+    H2mI::Matrix{Float64}
+    H2pI::LU{Float64} # LU
+    H2::Matrix{Float64}
+    H2xpf::Vector{Float64}
+    x::Vector{Float64}
+    y::Vector{Float64}
+    daqp_workspace::DAQPBase.Model
+    kkt_buffer::Vector{Float64}
+
+    settings::AVISettings
+end
+
+
+function setup_avi(H,f,A,bu,bl;x0 = zeros(0), daqp_workspace=nothing, settings=AVISettings())
+    n,m = length(f),length(bu)
+    bl = isempty(bl) ? fill(-1e30,m) : bl
+
+    H1 = (H+H') / 4
+    H2 = H1+(H-H') / 2
+    H1pI,H2mI,H2pI = H1+I, H2-I, lu!(H2+I)
+
+    x = isempty(x0) ? zeros(n) : copy(x0)
+    y,H2xpf = zeros(n),zeros(n)
+
+    kkt_buffer = Float64[]
+    sizehint!(kkt_buffer,4*(n+1)*(n+1))
+
+    daqp_workspace = isnothing(daqp_workspace) ? Model() : daqp_workspace;
+    DAQPBase.settings(daqp_workspace, Dict(:iter_limit => settings.inner_iter_limit, 
+                                           :primal_tol => settings.primal_tol, 
+                                           :dual_tol => settings.dual_tol))
+    exitflag,tsetup = DAQPBase.setup(daqp_workspace, H1pI, H2xpf, A, bu, bl)
+
+    return exitflag, AVIWorkspace(H,f,A,bu,bl,
+                                  H1pI,H2mI,H2pI,H2,H2xpf,
+                                  x,y,daqp_workspace,kkt_buffer,
+                                  settings)
+end
+
+function solve(ws::AVIWorkspace)
+    exitflag = -4
+    λstar, ASstar = zeros(0), Int[]
+    n = length(ws.f)
+    ϵp, ϵd = ws.settings.primal_tol, ws.settings.dual_tol
+    α,β = ws.settings.alpha, ws.settings.beta
+    tot_iter,outer_iter = 0,0
+    @inbounds for k in 1:ws.settings.iter_limit
+        ws.H2xpf .= ws.f
+        mul!(ws.H2xpf, ws.H2mI, ws.x, 1.0, 1.0)
+        DAQPBase.update(ws.daqp_workspace, nothing, ws.H2xpf, nothing, nothing,nothing)
+        ws.y[:], _, exitflag, info = DAQPBase.solve(ws.daqp_workspace)
+        exitflag < 0 && break 
+        tot_iter += info.iterations
+
+        # Same AS -> Check if KKT conditions are satisfied
+        if info.iterations == 1
+            ASu,ASl = _get_AS(info.λ,ϵd) 
+            z,AS = _solve_kkt(ws.H,ws.f,ws.A,ws.bu,ws.bl,ASu,ASl,ws.kkt_buffer)
+            xt = @view z[1:n]
+            λ = @view z[n+1:end]
+            nu = length(ASu)
+            if all(λ[i] > -ϵd for i in 1:nu) && all(λ[i] < ϵd for i in nu+1:length(λ))
+                Ax = ws.A*xt
+                if all(Ax-ws.bu .<= ϵp) && all(Ax-ws.bl .>=-ϵp)
+                    ws.x .= xt
+                    λstar,ASstar = copy(λ),AS
+                    exitflag, outer_iter = 1,k
+                    break
+                end
+            end
+            axpby!(β,xt,1.0-β,ws.x)
+        else
+            axpby!(1.0-α,ws.x,α,ws.y)
+            mul!(ws.y, ws.H2, ws.x, 1.0, 1.0)
+            ldiv!(ws.x, ws.H2pI, ws.y)
+        end
+    end
+    info = (status=flag2status[exitflag], AS=ASstar, outer_iterations=outer_iter, iterations=tot_iter)
+    return ws.x,λstar,exitflag,info
+end
+
 function _get_AS(λ,dual_tol)
     ASu,ASl = Int[],Int[]
     for i in 1:length(λ)
@@ -47,58 +145,7 @@ x ∈ {x: blower ≤ Ax ≤ bupper} such that:
 * `info` 		- tuple containing extra information from the solver such as status, active set and number of iterations.
 
 """
-function solve_avi(H::AbstractMatrix,f::AbstractVector,A::AbstractMatrix,bupper::AbstractVector,blower::AbstractVector=Float64[];
-        alpha=1.0,beta=0.25,primal_tol=1e-6, dual_tol = 1e-12, x0 = zeros(0), iter_limit=1000, inner_iter_limit=10000)
-
-    # Solves VI(Hx +h, C) where C={x| blower <= Ax <= bupper} using Douglas-Rachford
-    n,m = length(f),length(bupper)
-    blower = isempty(blower) ? fill(-1e30,m) : blower
-
-    H1 = (H+H') / 4
-    H2 = H1+(H-H') / 2
-    H1pI,H2mI,H2pI = H1+I, H2-I, lu!(H2+I)
-
-    x = isempty(x0) ? zeros(n) : copy(x0)
-    y,H2xpf = zeros(n),zeros(n),zeros(n)
-
-    backstep = DAQPBase.Model()
-    DAQPBase.settings(backstep, Dict(:iter_limit =>inner_iter_limit, :primal_tol=>primal_tol, :dual_tol=>dual_tol))
-    exitflag,tsetup = DAQPBase.setup(backstep, H1pI, H2xpf, A, bupper, blower)
-    exitflag < 0 && return nothing,nothing, (status=:Infeasible, exitflag=exitflag, outer_iterations=0, iterations=0) 
-
-    tot_iter = 0
-
-    kkt_buffer = Float64[]
-    sizehint!(kkt_buffer,4*(n+1)*(n+1))
-
-    @inbounds for k in 1:iter_limit
-        H2xpf .= f
-        mul!(H2xpf, H2mI, x, 1.0, 1.0)
-        DAQPBase.update(backstep, nothing, H2xpf, nothing, nothing,nothing)
-
-        y[:], _, exitflag, info = DAQPBase.solve(backstep)
-        exitflag < 0 && return nothing,nothing, (status=:Infeasible, exitflag=exitflag, outer_iterations=k, iterations=tot_iter) 
-        tot_iter += info.iterations
-
-        # Same AS -> Check if KKT conditions are satisfied
-        if info.iterations == 1
-            ASu,ASl = _get_AS(info.λ,dual_tol) 
-            z,AS = _solve_kkt(H,f,A,bupper,blower,ASu,ASl,kkt_buffer)
-            xt = @view z[1:n]
-            λ = @view z[n+1:end]
-            if all(λ[i] > -dual_tol for i in 1:length(ASu)) && all(λ[i] < dual_tol for i in length(ASu)+1:length(λ))
-                Ax = A*xt
-                if all(Ax-bupper .<= primal_tol) && all(Ax-blower .>=-primal_tol)
-                    return copy(xt),copy(λ),(AS=AS, status=:Solved, outer_iterations=k,iterations=tot_iter)
-                end
-            end
-            axpby!(beta,xt,1.0-beta,x)
-        else
-            axpby!(1.0-alpha,x,alpha,y)
-            mul!(y, H2, x, 1.0, 1.0)
-            ldiv!(x, H2pI, y)
-        end
-    end
-    return x,nothing, (status=:MaximumIterations, outer_iterations=iter_limit, iterations=tot_iter)
+function solve_avi(H::AbstractMatrix,f::AbstractVector,A::AbstractMatrix,bupper::AbstractVector,blower::AbstractVector=Float64[]; x0 = zeros(0), settings=AVISettings())
+    exitflag, ws = setup_avi(H,f,A,bupper,blower;x0=zeros(0),settings)
+    return solve(ws)
 end
-
