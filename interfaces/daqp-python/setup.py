620254388a337ccd6c009025d2cff8baf821d2e0
Daniel Arnström <55484604+darnstrom@users.noreply.github.com>
2026-03-17 23:20:45 +0100

    Bump version from 0.7.2 to 0.8.0 in Python Interface. (#114)

--- interfaces/daqp-python/setup.py
+++ interfaces/daqp-python/setup.py
@@ -42,11 +42,11 @@ cython_ext = Extension('daqp',
         include_dirs=['csrc/include'])
 
 setup(name='daqp',
-        version='0.7.2',
+        version='0.8.0',
         description='DAQP: A dual active-set QP solver',
         url='http://github.com/darnstrom/daqp',
         author='Daniel Arnström',
-        author_email='daniel.arnstrom@liu.se',
+        author_email='daniel.arnstrom@gmail.com',
         license='MIT',
         long_description=open('README.md','r').read(),
         long_description_content_type='text/markdown',
