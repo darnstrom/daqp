9e81b055168d288673ccb9668f5c5c5d3a9deaa6
darnstrom <daniel.arnstrom@gmail.com>
2026-07-26 19:59:58 +0200

    Restore relative path

--- interfaces/daqp-julia/test/utils.jl
+++ interfaces/daqp-julia/test/utils.jl
@@ -117,7 +117,7 @@ end
 function get_local_sources(srcdir)
     # Get local source
     # <repo>/interfaces/daqp-julia/test -> <repo>
-    daqp_dir = joinpath(dirname(@__FILE__), "..","..","..")
+    daqp_dir = joinpath(dirname(@__FILE__), "..","..","..","..")
     cfiles = ["daqp.c","auxiliary.c","factorization.c", "bnb.c", "hierarchical.c"]
     hfiles = ["daqp.h","auxiliary.h","factorization.h", "bnb.h", "hierarchical.h","constants.h", "types.h"]
     for cf in cfiles
